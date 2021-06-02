2f0b264b1a87e7c395ff9871590f48c01f2dec16
Siddharth Singh Baghel <siddharthsinghrewa5@gmail.com>
2021-06-02 13:28:02 +0530

    Adding the upcoming exercise in the RestView

--- app/src/main/java/com/siddharthsinghbaghel/healthyways/ExerciseActivity.kt
+++ app/src/main/java/com/siddharthsinghbaghel/healthyways/ExerciseActivity.kt
@@ -10,99 +10,163 @@ import kotlinx.android.synthetic.main.activity_exercise.*
 class ExerciseActivity : AppCompatActivity() {
 
     private var restTimer: CountDownTimer? = null
-    private var exerciseTimer: CountDownTimer? = null
     private var restProgress = 0
+
+    private var exerciseTimer: CountDownTimer? = null
     private var exerciseProgress = 0
 
     private var exerciseList: ArrayList<ExerciseModel>? = null
     private var currentExercisePosition = -1
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_exercise)
-        setSupportActionBar(toolbar_exercise_activity)
 
-        val actionBar = supportActionBar
-        if(actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true)
-        }
+        setSupportActionBar(toolbar_exercise_activity)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
         toolbar_exercise_activity.setNavigationOnClickListener {
             onBackPressed()
         }
 
-        setUpRestView()
-
         exerciseList = Constants.defaultExerciseList()
 
+        setupRestView()
     }
 
-    override fun onDestroy() {
-        if(restTimer != null)
-        {
+    /**
+     * Here is Destroy function we will reset the rest timer to initial if it is running.
+     */
+    public override fun onDestroy() {
+        if (restTimer != null) {
             restTimer!!.cancel()
             restProgress = 0
         }
 
+        if (exerciseTimer != null) {
+            exerciseTimer!!.cancel()
+            exerciseProgress = 0
+        }
         super.onDestroy()
     }
 
-  private fun setRestProgressBar(){
-      progressBar.progress = restProgress
-      restTimer = object: CountDownTimer(10000,1000){
-          override fun onTick(millisUntilFinished: Long) {
-              restProgress++
-              progressBar.progress = 10 - restProgress
-              tvTimer.text = (10 - restProgress).toString()
-          }
+    /**
+     * Function is used to set the timer for REST.
+     */
+    private fun setupRestView() {
 
-          override fun onFinish() {
-              Toast.makeText(this@ExerciseActivity, "Let's start exercising", Toast.LENGTH_SHORT).show()
+        // Here according to the view make it visible as this is Rest View so rest view is visible and exercise view is not.
+        llRestView.visibility = View.VISIBLE
+        llExerciseView.visibility = View.GONE
 
-              llRestView.visibility = View.GONE
-              setUpExerciseView()
-
-          }
-      }.start()
-  }
-
-    private fun setUpRestView(){
-        if(restTimer != null)
-        {
+        /**
+         * Here firstly we will check if the timer is running the and it is not null then cancel the running timer and start the new one.
+         * And set the progress to initial which is 0.
+         */
+        if (restTimer != null) {
             restTimer!!.cancel()
             restProgress = 0
         }
-      setRestProgressBar()
+
+        // TODO (Step 2 - Setting the upcoming exercise name in the UI element.)
+        // START
+        // Here we have set the upcoming exercise name to the text view
+        // Here as the current position is -1 by default so to selected from the list it should be 0 so we have increased it by +1.
+
+        // END
+
+        // This function is used to set the progress details.
+
+        tvUpCExerciseName.text = exerciseList!![currentExercisePosition + 1].getName()
+
+        setRestProgressBar()
+
+
     }
 
-    private fun setExerciseProgressBar(){
-        exerciseProgressBar.progress = exerciseProgress
-        exerciseTimer = object: CountDownTimer(30000,1000){
+    /**
+     * Function is used to set the progress of timer using the progress
+     */
+    private fun setRestProgressBar() {
 
+        progressBar.progress = restProgress // Sets the current progress to the specified value.
+
+        /**
+         * @param millisInFuture The number of millis in the future from the call
+         *   to {#start()} until the countdown is done and {#onFinish()}
+         *   is called.
+         * @param countDownInterval The interval along the way to receive
+         *   {#onTick(long)} callbacks.
+         */
+        // Here we have started a timer of 10 seconds so the 10000 is milliseconds is 10 seconds and the countdown interval is 1 second so it 1000.
+        restTimer = object : CountDownTimer(10000, 1000) {
             override fun onTick(millisUntilFinished: Long) {
-                exerciseProgress++
-                exerciseProgressBar.progress = 30 - exerciseProgress
-                println(exerciseProgressBar.progress)
-                tvExerciseTimer.text = (30 - exerciseProgress).toString()
+                restProgress++ // It is increased to ascending order
+                progressBar.progress = 10 - restProgress // Indicates progress bar progress
+                tvTimer.text =
+                        (10 - restProgress).toString()  // Current progress is set to text view in terms of seconds.
             }
 
             override fun onFinish() {
-                Toast.makeText(this@ExerciseActivity, "Exercising end", Toast.LENGTH_SHORT).show()
+                currentExercisePosition++
 
+                setupExerciseView()
             }
         }.start()
     }
 
-    private fun setUpExerciseView(){
-        if(exerciseTimer != null)
-        {
+    /**
+     * Function is used to set the progress of timer using the progress for Exercise View.
+     */
+    private fun setupExerciseView() {
+
+        // Here according to the view make it visible as this is Exercise View so exercise view is visible and rest view is not.
+        llRestView.visibility = View.GONE
+        llExerciseView.visibility = View.VISIBLE
+
+        /**
+         * Here firstly we will check if the timer is running the and it is not null then cancel the running timer and start the new one.
+         * And set the progress to initial which is 0.
+         */
+        if (exerciseTimer != null) {
             exerciseTimer!!.cancel()
             exerciseProgress = 0
         }
-        llExerciseView.visibility = View.VISIBLE
+
+        /**
+         * Here current exercise name and image is set to exercise view.
+         */
+        ivImage_Exercise.setImageResource(exerciseList!![currentExercisePosition].getImage())
+        tvExerciseName.text = exerciseList!![currentExercisePosition].getName()
+
         setExerciseProgressBar()
+    }
 
+    /**
+     * Function is used to set the progress of timer using the progress for Exercise View for 30 Seconds
+     */
+    private fun setExerciseProgressBar() {
 
+        progressBar.progress = exerciseProgress
 
+        exerciseTimer = object : CountDownTimer(30000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                exerciseProgress++
+                progressBar.progress = 30 - exerciseProgress
+                tvExerciseTimer.text = (30 - exerciseProgress).toString()
+            }
+
+            override fun onFinish() {
+                if (currentExercisePosition < 11) {
+                    setupRestView()
+                } else {
+
+                    Toast.makeText(
+                            this@ExerciseActivity,
+                            "Congratulations! You have completed the 7 minutes workout.",
+                            Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        }.start()
     }
 }
