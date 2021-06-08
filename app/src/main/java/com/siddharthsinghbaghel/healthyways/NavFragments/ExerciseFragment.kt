8b844125ef8785c820f865453430bd74ecc9ecbd
Siddharth Singh Baghel <siddharthsinghrewa5@gmail.com>
2021-06-08 22:51:06 +0530

    nav fragments created

---
+++ app/src/main/java/com/siddharthsinghbaghel/healthyways/NavFragments/ExerciseFragment.kt
@@ -0,0 +1,30 @@
+package com.siddharthsinghbaghel.healthyways.NavFragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.siddharthsinghbaghel.healthyways.R
+
+
+
+
+class ExerciseFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?,
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_exercise, container, false)
+    }
+
+
+}
